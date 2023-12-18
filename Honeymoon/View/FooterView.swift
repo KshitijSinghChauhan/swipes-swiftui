427a03ae66aa56db176f6a754b5900b9b865486c
Kshitij Singh Chauhan <kshitijsinghchauhan@gmail.com>
2023-12-18 16:46:22 +0530

    feature: added header and footer view

---
+++ Honeymoon/View/FooterView.swift
@@ -0,0 +1,48 @@
+//
+//  FooterView.swift
+//  Honeymoon
+//
+//  Created by Kshitij Singh Chauhan on 18/12/23.
+//
+
+import SwiftUI
+
+struct FooterView: View {
+    
+    var body: some View {
+        HStack {
+            Image(systemName: "xmark.circle")
+                .font(.system(size: 42, weight: .light))
+            
+            Spacer()
+            
+            Button {
+                // ACTION
+                print("success")
+            } label: {
+                Text("Book Destination")
+                    .font(.system(.subheadline, design: .rounded, weight: .heavy))
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 12)
+                    .accentColor(Color.pink)
+                    .background(
+                        Capsule().stroke(Color.pink, lineWidth: 2)
+                    )
+            }
+
+            
+            Spacer()
+
+            Image(systemName: "heart.circle")
+                .font(.system(size: 42, weight: .light))
+        } //: HSTACK
+        .padding()
+    }
+}
+
+struct FooterView_Previews: PreviewProvider {
+    static var previews: some View {
+        FooterView()
+            .previewLayout(.fixed(width: 375, height: 80))
+    }
+}
