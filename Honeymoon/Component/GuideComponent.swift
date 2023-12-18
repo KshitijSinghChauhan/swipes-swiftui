c070f50ae877e11bf219acb932339c723d3b73fe
Kshitij Singh Chauhan <kshitijsinghchauhan@gmail.com>
2023-12-18 19:01:14 +0530

    feature: added guide view

--- Honeymoon/Component/GuideComponent.swift
+++ Honeymoon/Component/GuideComponent.swift
@@ -48,7 +48,6 @@ struct GuideComponent: View {
                 
             } //: VSTACK
         } //: HSTACK
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
---
+++ Honeymoon/View/GuideView.swift
@@ -0,0 +1,83 @@
+//
+//  GuideView.swift
+//  Honeymoon
+//
+//  Created by Kshitij Singh Chauhan on 18/12/23.
+//
+
+import SwiftUI
+
+struct GuideView: View {
+    
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .center, spacing: 20) {
+                HeaderComponent()
+                
+                Spacer(minLength: 10)
+                
+                Text("Get started")
+                    .fontWeight(.black)
+                    .font(.largeTitle)
+                    .foregroundColor(Color.pink)
+                
+                Text("Discover and pick the perfect destination for your romantic honeymoon!")
+                    .lineLimit(nil)
+                    .multilineTextAlignment(.center)
+                
+                Spacer(minLength: 10)
+                
+                VStack(alignment: .leading, spacing: 25) {
+                    GuideComponent(
+                        title: "Like",
+                        subTitle: "Swipe right",
+                        description: "Do you like this destination? Touch the screen and swipe right. It will be saved to the favourites.",
+                        icon: "heart.circle"
+                    )
+                    
+                    GuideComponent(
+                        title: "Dismiss",
+                        subTitle: "Swipe left",
+                        description: "Would you rather skip this place? Touch the screen and swipe left. You will no longere it.",
+                        icon: "xmark.circle"
+                    )
+                    
+                    GuideComponent(
+                        title: "Book",
+                        subTitle: "Tap the button",
+                        description: "Our selection of honeymoon resorts is perfect settingfor you to embark your new life together.",
+                        icon: "checkmark.square"
+                    )
+                }
+                
+                Spacer(minLength: 10)
+                
+                Button {
+                    // ACTION
+                    print("A button was tapped.")
+                } label: {
+                    Text("Continue".uppercased())
+                        .font(.headline)
+                        .padding()
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .background(
+                            Capsule()
+                                .fill(Color.pink)
+                        )
+                        .foregroundColor(Color.white)
+                }
+
+            }
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .padding(.top, 15)
+            .padding(.bottom, 25)
+            .padding(.horizontal, 25)
+        } //: VSTACK
+    }
+}
+
+struct GuideView_Previews: PreviewProvider {
+    static var previews: some View {
+        GuideView()
+    }
+}
